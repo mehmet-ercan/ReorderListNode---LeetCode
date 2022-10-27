c1695f20621d154ff6f769c8b5ce3e7ddd620d8c
abmehmet <mehmetercan5755@gmail.com>
2022-10-27 15:43:16 +0300

    start ve end ile geliştirilmiş uygulama eklendi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,73 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!_");
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
+
+    public static void main(String[] args) {
+
+        ListNode head0 = new ListNode(0);
+        ListNode head1 = new ListNode(1);
+        ListNode head2 = new ListNode(2);
+        ListNode head3 = new ListNode(3);
+        ListNode head4 = new ListNode(4);
+        head0.next = head1;
+        head1.next = head2;
+        head2.next = head3;
+        head3.next = head4;
+
+        reorderList(head0);
+
+        while (head0.next != null) {
+            System.out.println(head0.val + " ");
+            head0 = head1.next;
+        }
+    }
+
+    public static void reorderList(ListNode head) {
+        if (head.next != null) {
+            ListNode start = head.next;
+            ListNode end = getLastNode(head);
+            ListNode tempHead = head;
+
+            while (start != end) {
+                if (start.next == end) {
+                    break;
+                }
+
+                tempHead.next = end;
+                end.next = start;
+                tempHead = tempHead.next;
+                tempHead = tempHead.next;
+                start = start.next;
+                end = getLastNode(start);
+
+            }
+        }
+    }
+
+    public static ListNode getLastNode(ListNode l) {
+        ListNode btl = l;
+
+        while (l.next != null) {
+            btl = l;
+            l = l.next;
+        }
+
+        btl.next = null;
+        return l;
+    }
+
 }
